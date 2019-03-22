1bb1d57a13dc8615ebe99b93d34e695118535f7a
cruel-32 <cruel32@nate.com>
2019-03-22 18:02:44 +0900

    js code

--- gulp/source/js/20190321/1.js
+++ gulp/source/js/20190321/1.js
@@ -1,44 +1,8 @@
 'use strict'
 
-const config = {
-    entryDir : ''
-}
-const dirList = [
-    `\\project1\\build\\html\\test.html`,
-    `\\project2\\build\\html\\20180318\\functional-programming-1`,
-    `\\project3\\build\\html\\20180318\\functional-programming-2`,
-    `\\project4\\build\\html\\20181017\\flex_test.html`,
-    `\\project5\\build\\html\\20190321\\structures&algorithms.html`,
-];
+const a = 1;
+const b = 2;
+const c = 3;
 
-const maps = [];
-dirList.forEach(dir=>{
-    const allPath = dir.replace(config.entryDir, '').split('\\').reduce((arr,path)=>{
-        if(path){
-            arr.push(path);
-        }
-        return arr
-    },[])
-    const buildFolder = (folders, depth)=>{
-        if((depth+1) >= allPath.length){
-            folders.push(allPath[depth]);
-        } else {
-            const path = allPath[depth];
-            const folder = folders.find(f=>f.folderName===path);
-            if(folder){
-                buildFolder(folder.files, depth+1);
-            } else {
-                const files = [];
-                folders.push({
-                    "folderName" : path,
-                    "parentPath" : allPath[depth-1] || null,
-                    files
-                });
-                buildFolder(files,depth+1);
-            }
-        }
-    }
-    buildFolder(maps,0);
-});
-
-console.log('maps : ', maps)
+console.log('a & b : ', (a & b) & c);
+console.log('a | b : ', (a | b));
