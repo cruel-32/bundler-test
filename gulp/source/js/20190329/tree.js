7ae50df639a501122e3b7e986b7b054af9031984
cruel-32 <cruel32@nate.com>
2019-03-30 17:19:31 +0900

    graph 자료형 추가중

--- gulp/source/js/20190329/tree.js
+++ gulp/source/js/20190329/tree.js
@@ -34,9 +34,6 @@ class BinarySearchTree {
             }
         }
     }
-    search(){
-
-    }
     inOrderTraverse(callback){
         this.inOrderTraverseNode(this.root,callback);
     }
@@ -57,10 +54,100 @@ class BinarySearchTree {
             this.preOrderTraverseNode(node.right, callback);
         }
     }
-    postOrderTraverse(){}
-    min(){}
-    max(){}
-    remove(){}
+    postOrderTraverse(callback){
+        this.postOrderTraverseNode(this.root, callback);
+    }
+    postOrderTraverseNode(node, callback){
+        if(node !== null){
+            this.postOrderTraverseNode(node.left, callback);
+            this.postOrderTraverseNode(node.right, callback);
+            callback(node.key);
+        }
+    }
+    min(){
+        return this.minNode(this.root);
+    }
+    minNode(node){
+        if(node){
+            while(node && node.left !== null){
+                node = node.left
+            }
+            return node.key
+        }
+        return null;
+    }
+    max(){
+        return this.maxNode(this.root);
+    }
+    maxNode(node){
+        if(node){
+            while(node && node.right !== null){
+                node = node.right
+            }
+            return node.key
+        }
+        return null;
+    }
+    search(key){
+        return this.searchNode(this.root, key);
+    }
+    searchNode(node, key){
+        if(node === null){
+            return false;
+        }
+        if(key < node.key){
+            return this.searchNode(node.left, key);
+        } else if(key > node.key){
+            return this.searchNode(node.right,key);
+        } else {
+            return true;
+        }
+    }
+    remove(key){
+        this.root = this.removeNode(this.root, key);
+    }
+    removeNode(node,key){
+        if(node === null){
+            return null;
+        }
+        if(key < node.key){
+            node.left = this.removeNode(node.left, key);
+            return node;
+        } else if(key > node.key){
+            node.right = this.removeNode(node.right, key);
+            return node;
+        } else {
+            //case 1 - 리프 노드
+            if(node.left === null && node.right === null){
+                node = null;
+                return node;
+            }
+
+            //case 2 - 자식이 하나뿐인 노드
+            if(node.left === null){
+                node = node.right;
+                return node;
+            } else if(node.right === null){
+                node = node.left;
+                return node;
+            }
+
+            //case 3 - 자식이 둘인 노드
+            const aux = this.findMinNode(node.right);
+            node.key = aux.key;
+            node.right = this.removeNode(node.right, aux.key);
+            return node;
+        }
+    }
+    minNode(node){
+        if(node){
+            while(node && node.left !== null){
+                node = node.left
+            }
+            return node
+        }
+        return null;
+    }
     printNode(value){
         console.log(value);
     }
@@ -88,4 +175,8 @@ console.log('tree : ', tree);
 
 tree.inOrderTraverse(tree.printNode);
 tree.preOrderTraverse(tree.printNode);
+
+console.log(tree.search(1) ? '찾았다' : '못찾았다');
+console.log(tree.search(8) ? '찾았다' : '못찾았다');
+
 
---
+++ gulp/source/js/20190330/graph.js
@@ -0,0 +1,89 @@
+
+'use strict'
+//tree
+function Graph(){
+    let vertices = [];
+    let adjList = new Dictionary();
+
+    const initializeColor = ()=>{
+        let color = [];
+        for(let i=0; i<vertices.length; i++){
+            color[vertices[i]] = 'white';
+        }
+        return color;
+    }
+
+    this.addVertex = (v)=>{
+        vertices.push(v);
+        adjList.set(v,[]);
+    }
+
+    this.addEdge = (v,w)=>{
+        adjList.get(v).push(w);
+        adjList.get(w).push(v);
+    }
+
+    this.toString = ()=>{
+        let s = '';
+        for(let i=0; i<vertices.length; i++){
+            s += vertices[i] + ' -> ';
+            let neighbors = adjList.get(vertices[i]);
+            for(let j=0; j<neighbors.length; j++){
+                s += neighbors[j];
+            }
+            s += '\n';
+        }
+        return s;
+    }
+
+    this.bfs = (v, callback)=>{
+        let color = initializeColor(),
+            queue = new Queue();
+        
+        queue.enqueue(v);
+
+        while(!queue.isEmpty()){
+            let u = queue.dequeue(),
+                neighbors = adjList.get(u);
+            
+            color[u] = 'grey';
+            
+            for(let i=0; i<neighbors.length; i++){
+                let w = neighbors[i];
+                if(color[w] === 'white'){
+                    color[w] = 'grey';
+                    queue.enqueue(w);
+                }
+            }
+            color[u] = 'black';
+            if(callback){
+                callback(u);
+            }
+        }
+    }
+    this.printNode = (value)=>{
+        console.log('탐색한 정점 : ', value);
+    }
+}
+
+const graph = new Graph();
+const myVertices = ['A','B','C','D','E','F','G','H','I'];
+
+for(let i=0; i<myVertices.length; i++){
+    graph.addVertex(myVertices[i]);
+}
+
+graph.addEdge('A','B');
+graph.addEdge('A','C');
+graph.addEdge('A','D');
+graph.addEdge('C','D');
+graph.addEdge('C','G');
+graph.addEdge('D','G');
+graph.addEdge('D','H');
+graph.addEdge('B','E');
+graph.addEdge('B','F');
+graph.addEdge('E','I');
+
+console.log(graph.toString());
+
+graph.bfs(myVertices[0], graph.printNode);
